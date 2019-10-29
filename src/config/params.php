1ffb27c4f4105d15508743bd4a5ab2d6e4040f18
viktorprogger <viktorprogger@gmail.com>
2019-10-29 14:04:39 +0300

    Remove channels from vcs

--- src/config/params.php
+++ src/config/params.php
@@ -1,34 +1,7 @@
 <?php
 declare(strict_types=1);
 
-use rssBot\dto\StormItem;
-
 return [
     'bot_token' => getenv('BOT_TOKEN'),
-    'rss_channels' => [
-        [
-            'chat_id' => '@vitorprogger_php_info',
-            'links'   => [
-                [
-                    'title'     => 'JetBrains PhpStorm',
-                    'url'       => 'https://blog.jetbrains.com/phpstorm/feed/',
-                    'itemClass' => StormItem::class,
-                ],
-                [
-                    'title'     => 'JetBrains YouTrack',
-                    'url'       => 'https://blog.jetbrains.com/youtrack/feed/',
-                    'itemClass' => StormItem::class,
-                ],
-                [
-                    'title'     => 'JetBrains TeamCity',
-                    'url'       => 'https://blog.jetbrains.com/teamcity/feed/',
-                    'itemClass' => StormItem::class,
-                ],
-                [
-                    'title'     => 'Пятиминутка PHP',
-                    'url'       => 'http://feeds.soundcloud.com/users/soundcloud:users:153519653/sounds.rss',
-                ],
-            ],
-        ],
-    ],
+    'rss_channels' => require __DIR__ . '/channels.php',
 ];
