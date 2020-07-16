f1eb3e1ba7b41db38e477d0e5f46eaa97fefe81f
viktorprogger <viktorprogger@gmail.com>
2020-07-16 17:15:21 +0300

    Fix tg formatter

--- src/services/formatters/TelegramDefaultFormatter.php
+++ src/services/formatters/TelegramDefaultFormatter.php
@@ -10,6 +10,7 @@ class TelegramDefaultFormatter implements FormatterInterface
     public function formatMessage(FeedItemInterface $item): string
     {
         $body = $item->getBody();
+        $body = preg_replace('/([*_\[\]()])/', '\\$1', $body);
 
         $result = '*' . $item->getTitle() . "*\n";
         $result .= '_' . $item->getSiteName() . "_\n\n";
