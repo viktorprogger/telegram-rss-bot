5ea799dc708f265bcca178b0751d30810ae659f1
viktorprogger <viktorprogger@gmail.com>
2020-07-29 19:13:22 +0300

    Adjust for yii-queue changes, part 3

--- src/queue/handlers/SourceFetcher.php
+++ src/queue/handlers/SourceFetcher.php
@@ -45,7 +45,7 @@ class SourceFetcher
         foreach ($source->getItems() as $item) {
             foreach ($this->repository->getBySource($source) as $sender) {
                 if ($sender->suits($item)) {
-                    $this->queue->push(new SendItemJob($item));
+                    $this->queue->push(new SendItemJob($item, $sender->getCode()));
                 }
             }
         }
