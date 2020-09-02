18f3f6962254cb28ee9ad7b285d2d9db665cba19
viktorprogger <viktorprogger@gmail.com>
2020-09-02 13:08:48 +0300

    Simplify configuration

--- src/config/actions.php
+++ src/config/actions.php
@@ -3,34 +3,22 @@
 declare(strict_types=1);
 
 use rssBot\neww\ActionInterface;
-use rssBot\neww\Listener;
-use Yiisoft\Validator\Result;
-use Yiisoft\Validator\Rules;
 
 return [
     ActionInterface::class => [LogAction::class],
     OpenDoorAction::class => [
         [
-            '__class' => Listener::class,
-            '__construct()' => [
-                'actionDefinition' => TurnTeapotOnAction::class,
-                'synchronous' => false,
-            ],
+            'action' => TurnTeapotOnAction::class,
+            'synchronous' => false,
         ],
         [
-            '__class' => Listener::class,
-            '__construct()' => [
-                'actionDefinition' => TurnLightOnAction::class,
-                'synchronous' => false,
-            ],
+            'action' => TurnLightOnAction::class,
+            'synchronous' => false,
         ],
     ],
-    CheckFingerprintAction => static function() {
-        $failResult = (new Result());
-        $failResult->addError('Fingerprint not found');
-
-        $rules = [fn(bool $value) => $value ? new Result() : $failResult];
-
-        return new Listener(OpenDoorAction::class, new Rules($rules), false);
-    },
+    CheckFingerprintAction => [
+        'action' => OpenDoorAction::class,
+        'condition' => fn(bool $value) => $value,
+        'synchronous' => false,
+    ],
 ];
