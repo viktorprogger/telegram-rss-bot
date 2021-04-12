17fe599c6b51f9ee40b3b7b3ebe61ca418ba1240
viktorprogger <viktorprogger@gmail.com>
2021-04-12 14:46:41 +0300

    A bit of debugging

--- app/public/index.php
+++ app/public/index.php
@@ -4,6 +4,8 @@ declare(strict_types=1);
 
 use Resender\Infrastructure\Web\ApplicationRunner;
 
+file_put_contents(dirname(__DIR__) . '/runtime/logs/test.php', date('[Y-m-d H:i:s]') . '  test');
+
 require_once dirname(__DIR__) . '/vendor/autoload.php';
 
 $runner = new ApplicationRunner();
