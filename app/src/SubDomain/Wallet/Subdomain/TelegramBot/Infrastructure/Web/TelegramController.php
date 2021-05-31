5caccc5d9a76b3c24e16c7a70b2389224f821c69
viktorprogger <viktorprogger@gmail.com>
2021-05-31 11:04:38 +0300

    idle: remove comments

--- app/src/SubDomain/Wallet/Subdomain/TelegramBot/Infrastructure/Web/TelegramController.php
+++ app/src/SubDomain/Wallet/Subdomain/TelegramBot/Infrastructure/Web/TelegramController.php
@@ -23,15 +23,6 @@ final class TelegramController
 
     public function webhook(ServerRequestInterface $request): ResponseInterface
     {
-        // TODO Вынести все TODO в Space в задачи
-
-        // TODO TelegramRequestFactory, собирающий реквест из ServerRequestInterface
-        // TODO StateService - получает и персистит стейт для юзера
-        // TODO Обработчик для сообщения в очереди, который будет вызывать роутинг
-        // TODO Надо после формирования ответного сообщения как-то добавлять его снова в очередь. Мидлварью, думаю.
-        // TODO Нужна новая реализация фабрики каналов очереди, которая будет играться с обменниками
-        // TODO Прикрутить php-accounting: добавить в зависимости и понаделать ему orm-моделек
-        // TODO Реализовать все действия всех стейтов
 
         $telegramRequest = new TelegramRequest(
             $userId,
