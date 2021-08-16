3498dcb70c5d8f5b38915b0072dea434ad92e37c
viktorprogger <viktorprogger@gmail.com>
2021-08-16 16:32:18 +0300

    FIX: don't remove queue after a source processing because it's a cause of amqp channels run out

--- app/src/SubDomain/Rss/Domain/SourceProcessor.php
+++ app/src/SubDomain/Rss/Domain/SourceProcessor.php
@@ -7,19 +7,21 @@ namespace Resender\SubDomain\Rss\Domain;
 use Resender\SubDomain\Rss\Domain\Source\SourceInterface;
 use Resender\SubDomain\Rss\Infrastructure\QueueHandler\TargetHandler;
 use Yiisoft\Yii\Queue\Message\Message;
+use Yiisoft\Yii\Queue\Queue;
 use Yiisoft\Yii\Queue\QueueFactory;
 
 final class SourceProcessor
 {
+    private Queue $queue;
+
     public function __construct(
         private QueueFactory $queueFactory,
     ) {
+        $this->queue = $this->queueFactory->get(TargetHandler::CHANNEL_NAME);
     }
 
     public function process(SourceInterface $source): void
     {
-        $queue = $this->queueFactory->get(TargetHandler::CHANNEL_NAME);
-
         foreach ($source->getItems() as $item) {
             $itemArray = [
                 'sourceTitle' => $item->getSourceTitle(),
@@ -35,7 +37,7 @@ final class SourceProcessor
                     'item' => $itemArray,
                     'sourceId' => $source->getId(),
                 ];
-                $queue->push(new Message(TargetHandler::MESSAGE_NAME, $data));
+                $this->queue->push(new Message(TargetHandler::MESSAGE_NAME, $data));
             }
         }
     }
